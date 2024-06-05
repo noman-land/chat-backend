29e29a1d949f02691b98823bc181e6ca196e607a
Noman <noman@noman.land>
2024-06-05 19:45:50 -0400

    Return only 100 messages on POST

--- src/Chatroom.ts
+++ src/Chatroom.ts
@@ -1,30 +1,31 @@
 import { DurableObject } from 'cloudflare:workers';
 
 type Message = {
-	author: string;
-	message: string;
-	timestamp: number;
+  author: string;
+  message: string;
+  timestamp: number;
 };
 
 export class Chatroom extends DurableObject {
-	state: DurableObjectState;
-	env: Env;
+  state: DurableObjectState;
+  env: Env;
 
-	constructor(state: DurableObjectState, env: Env) {
-		super(state, env);
-		this.state = state;
-		this.env = env;
-	}
+  constructor(state: DurableObjectState, env: Env) {
+    super(state, env);
+    this.state = state;
+    this.env = env;
+  }
 
-	async getMessages() {
-		const messages = (await this.state.storage.get<Message[]>('messages')) || [];
-		return { messages };
-	}
+  async getMessages() {
+    const messages = (await this.state.storage.get<Message[]>('messages')) || [];
+    return { messages };
+  }
 
-	async sendMessage(message: Message) {
-		const messages = (await this.state.storage.get<Message[]>('messages')) || [];
-		messages.push(message);
-		await this.state.storage.put('messages', messages.slice(-100));
-		return { messages };
-	}
+  async sendMessage(message: Message) {
+    const latestMessages = (await this.state.storage.get<Message[]>('messages')) || [];
+    latestMessages.push(message);
+    const messages = latestMessages.slice(-100);
+    await this.state.storage.put('messages', messages);
+    return { messages };
+  }
 }
